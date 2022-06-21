cd74c9754f1d70fa2f3ebf9f595de5f215fc7c11
Adedipupo <ajokanola@terragonltd.com>
2022-06-21 03:52:35 +0100

    navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,18 @@
+import { ArrowDropDownOutlined, LaptopWindows, Notifications, SearchOutlined } from "@material-ui/icons";
+import { useState } from "react";
 import "./navbar.scss";
 
 const Navbar = () => {
+    const [isScrolled,setIsScrolled] = useState(false);
+
+    window.onscroll = () => {
+        setIsScrolled(window.pageYOffset === 0 ? false : true);
+        return () => (window.scroll = null)
+
+    }
+    console.log(isScrolled);
   return (
-    <div className="navbar">
+    <div className={isScrolled ? "navbar scrolled" : "navbar"}>
       <div className="container">
           <div className="left">
               <img src="https://cdn.iconscout.com/icon/free/png-256/netflix-3628944-3030169.png" alt="logo"/>
@@ -12,7 +22,19 @@ const Navbar = () => {
               <span>New and Popular</span>
               <span>My List</span>
           </div>
-          <div className="right"></div>
+          <div className="right">
+              <SearchOutlined className="icon"/>
+              <span>KIDS</span>
+              <Notifications className="icon"/>
+              <img src="https://cdn.playdislyte.com/wp-content/uploads/2022/04/Tevor-Sphinx-avatar.png" alt="user" />
+            <div className="profile">
+             <ArrowDropDownOutlined className="icon"/>
+             <div className="options">
+                 <span>Settings</span>
+                 <span>Logout</span>
+             </div>
+            </div>
+          </div>
       </div>
     </div>
   )
