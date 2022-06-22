f9e003a33f0f9b58866e45473f54593fbf6fa95c
Adedipupo <ajokanola@terragonltd.com>
2022-06-22 03:27:40 +0100

    featured

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,9 +1,29 @@
 import { InfoOutlined, PlayArrow } from "@material-ui/icons";
 import "./featured.scss";
 
-const Featured = () => {
+export default function Featured({type}){
   return (
     <div className="featured">
+        {type && (
+            <div className="category">
+                <span>{type === "movie" ? "Movies" : "Services"}</span>
+                <select name="genre" id="genre">
+                    <option>Genre</option>
+                    <option value="adventure">Adventure</option>
+                    <option value="comedy">Comedy</option>
+                    <option value="crime">Crime</option>
+                    <option value="historical">Historical</option>
+                    <option value="horror">Horror</option>
+                    <option value="romance">Romance</option>
+                    <option value="sci-fi">Sci-fi</option>
+                    <option value="thriller">Thriller</option>
+                    <option value="western">Western</option>
+                    <option value="animation">Animation</option>
+                    <option value="drama">Drama</option>
+                    <option value="documentry">Documentary</option>
+                </select>
+            </div>
+        )}
             <img src="https://venturebeat.com/wp-content/uploads/2020/04/ff7remake.jpg?fit=3341%2C1871&strip=all" alt="user" width="100%" />
 
             <div className="info">
@@ -26,4 +46,4 @@ const Featured = () => {
   )
 }
 
-export default Featured
+
