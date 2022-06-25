b9b51818894f3cd3eddb091d7e455a53ac39c634
Adedipupo <ajokanola@terragonltd.com>
2022-06-25 13:04:18 +0100

    ilistiteminfo

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,27 +1,33 @@
 import { ArrowBackIosOutlined, ArrowForwardIosOutlined } from "@material-ui/icons"
-import { useRef } from "react"
+import { useRef, useState } from "react"
 import ListItem from "../listItem/ListItem"
 import "./list.scss"
 
 export default function List() {
+    const [isMoved, setIsMoved] = useState(false);
+    const [slideNum, setSlideNum] = useState(0);
 
     const listRef = useRef();
     const handleClick = (direction) => {
+        setIsMoved(true);
         let distance = listRef.current?.getBoundingClientRect().x ?? - 50;
-        if (direction === "left") {
+        if (direction === "left" && slideNum > 0) {
+            setSlideNum(slideNum - 1);
             listRef.current.style.transform = `translateX(${230 + distance}px)`
         }
-        if (direction === "right") {
+        if (direction === "right" && slideNum < 5) {
+            setSlideNum(slideNum + 1)
             listRef.current.style.transform = `translateX(${-230 + distance}px)`
         }
     }
 
   return (
     <div className="list">
-      <span className="listTitle">Continue to watxh</span>
+      <span className="listTitle">Continue to watch</span>
       <div className="wrapper">
-          <ArrowBackIosOutlined className="sliderArrow left" onClick={()=>handleClick("left")}/>
+          <ArrowBackIosOutlined className="sliderArrow left" onClick={()=>handleClick("left")} style={{display: !isMoved && "none"}}/>
           <div className="container" ref={listRef}>
+              <ListItem/>
               <ListItem/>
               <ListItem/>
               <ListItem/>
--- src/components/listItem/ListItem.jsx
+++ src/components/listItem/ListItem.jsx
@@ -1,9 +1,18 @@
+import { Add, PlayArrow, ThumbDownOutlined, ThumbUpOutlined } from "@material-ui/icons";
 import "./listitem.scss";
 
 export default function ListItem() {
   return (
     <div className="listItem">
-      item
+      <img src="https://images.indulgexpress.com/uploads/user/imagelibrary/2019/4/13/w600X390/GameofThronesfordummiesItsnevertoolatetounderstandseriesplottwist.jpeg" />
+     <div className="itemInfo">
+         <div className="icons">
+             <PlayArrow/>
+             <Add />
+             <ThumbUpOutlined/>
+             <ThumbDownOutlined/>
+         </div>
+     </div>
     </div>
   )
 }
